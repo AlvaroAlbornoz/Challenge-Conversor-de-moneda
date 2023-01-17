035a179da90ed59a5fd500a9653a906218081ab2
AlvaroAlbornoz <ingematica.ltda@gmail.com>
2023-01-17 13:04:25 -0300

    Manejo de excepciones

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -6,64 +6,79 @@ import conversorMonedas.function;
 import conversorTemperatura.functionTemperatura;
 
 public class Principal {
-    public static void main (String[] args) {
+	public static void main(String[] args) {
 
-        function monedas = new function();
-        functionTemperatura temperatura = new functionTemperatura();
-             
-        while(true) {
-        	
-        	String opciones = (JOptionPane.showInputDialog(null, "Seleccione una opción de conversión ", "Menu", JOptionPane.QUESTION_MESSAGE, null, new Object[] {"Conversor de Moneda", "Conversor de Temperatura"}, "Seleccion")).toString();
+		function monedas = new function();
+		functionTemperatura temperatura = new functionTemperatura();
+		String opciones = "";
+		while (true) {
+			try{
+			opciones = (JOptionPane.showInputDialog(null, "Seleccione una opción de conversión ", "Menu",
+					JOptionPane.QUESTION_MESSAGE, null,
+					new Object[] { "Conversor de Moneda", "Conversor de Temperatura" }, "Seleccion")).toString();
+			}catch(NullPointerException e) {
+				System.out.println("Cancelado");
+				System.out.println(e);
+				System.exit(0);
+			}
+			
+			switch (opciones) {
+			case "Conversor de Moneda":
 
-        	switch(opciones) {
-        	case "Conversor de Moneda":
-        		 String input = JOptionPane.showInputDialog("Ingresa la cantidad de dinero que deseas convertir: ");
-                 if(ValidarNumero(input) == true) {
-                     double Minput = Double.parseDouble(input);
-                     monedas.ConvertirMonedas(Minput);
+				String input = JOptionPane.showInputDialog("Ingresa la cantidad de dinero que deseas convertir: ");
+			
+				if ( input != null && ValidarNumero(input) == true ) {
 
-                     int respuesta=JOptionPane.showConfirmDialog(null,"¿Deseas realizar otra conversión?");
-                     if (JOptionPane.OK_OPTION == respuesta){
-                     	System.out.println("Selecciona opción Afirmativa");
-                     }else{
-                        	JOptionPane.showMessageDialog(null, "Programa terminado");                         
-                     }
+					double Minput = Double.parseDouble(input);
+					monedas.ConvertirMonedas(Minput);
 
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Valor inválido");                
-                    }
-                     	break;
-        	
+					int respuesta = JOptionPane.showConfirmDialog(null, "¿Deseas realizar otra conversión?");
+					if (JOptionPane.OK_OPTION == respuesta) {
+						System.out.println("Selecciona opción Afirmativa");
+					} else {
+						JOptionPane.showMessageDialog(null, "Programa terminado");
+						System.exit(0);
+					}
 
-        	 case "Conversor de Temperatura":
-        	 input = JOptionPane.showInputDialog("Ingresa el valor de la temperatura que deseas convertir ");
-             if(ValidarNumero(input) == true) {
-                 double Minput = Double.parseDouble(input);
-                 temperatura.ConvertirTemperatura(Minput);
+				} else {
+					JOptionPane.showMessageDialog(null, "Valor inválido");
+				}
+				break;
 
-                 int respuesta = 0;
-                 respuesta = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
-                 if((respuesta == 0) && (ValidarNumero(input) == true)) { 
-                 } else {
-                     JOptionPane.showMessageDialog(null, "Programa terminado");                     
-                 }
+			case "Conversor de Temperatura":
+				input = JOptionPane.showInputDialog("Ingresa el valor de la temperatura que deseas convertir ");
+				System.out.println(input);
+				if (input != null && ValidarNumero(input) == true) {
+					double Minput = Double.parseDouble(input);
+					temperatura.ConvertirTemperatura(Minput);
 
-             } else {
-                 JOptionPane.showMessageDialog(null, "Valor inválido");                
-             }
-             break;
-        }                
-    }
-               
-  }
-    public static boolean ValidarNumero(String input) {
-        try {
-            double x = Double.parseDouble(input);
-            if(x >= 0 || x < 0);
-                return true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-    }
+					int respuesta = 0;
+					respuesta = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
+					if ((respuesta == 0) && (ValidarNumero(input) == true)) {
+					} else {
+						JOptionPane.showMessageDialog(null, "Programa terminado");
+						System.exit(0);
+					}
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Valor inválido");
+				}
+				break;
+			}
+		}
+
+	}
+
+	public static boolean ValidarNumero(String input) {
+		try {
+			double x = Double.parseDouble(input);
+			if (x >= 0 || x < 0)
+				;
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+	
+	
 }
-        	
